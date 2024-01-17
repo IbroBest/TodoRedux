fae92614928f11c3d45123629281cf71a8aec3ba
IbroBest <134087016+SultonbekovS@users.noreply.github.com>
2024-01-17 22:02:12 +0500

    dasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Text, Box,Heading } from "@chakra-ui/layout";
+import { HeadTodo } from "./HeadTodo";
 
-export const App = () => {
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Box  display={"flex"} flexDirection={'column'} alignItems={"center"} w={"100%"}>
+      <Heading fontSize={'30px'} my={'30px'} >create by ibrohim 🧑🏻‍💻</Heading>
+      <HeadTodo/>
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/HeadTodo.jsx
@@ -0,0 +1,36 @@
+import { Box } from "@chakra-ui/layout";
+import { Button, Input,Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+export const HeadTodo = () => {
+  const [inputVal, setInputVal] = useState("");
+  const [data, setData] = useState([]);
+  const infarmation = (event) => {
+    setInputVal(event.target.value);
+  };
+  const handleVal = () => {
+     setData([...data , { id: data.length + 1 , input: inputVal }
+    
+    ]);
+  };
+  return (
+    <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+      <Box display={"flex"} gap={"10px"}>
+        <Input onChange={infarmation} w={"400px"} />
+        <Button onClick={handleVal} colorScheme="green">
+          Add
+        </Button>
+      </Box>
+      <Box my={'20px'}>
+
+        {
+          data.map((value)=>(
+            <Box key={value.id} my={'10px'}  border={'0.5px solid grey'} padding={'7px'} display={'flex'} justifyContent={'space-between'}w={'400px'} alignItems={'center'}>
+              <Text fontSize={'18px'}>{value.id} :  {value.input}</Text>
+            <Button colorScheme="red">Delete</Button>
+        </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
